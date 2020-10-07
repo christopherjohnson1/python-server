e35d4b62b3a289ad786b6f46abdeffeab86149ba
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-07 11:32:16 -0400

    added files for python server

---
+++ animals/request.py
@@ -0,0 +1,37 @@
+ANIMALS = [
+    {
+      "id": 3,
+      "name": "Nilla",
+      "breed": "Golden Retriever",
+      "locationId": 3,
+      "treatment": "Nails trimmed, and ears cleaned.",
+      "customerId": 5
+    },
+    {
+      "id": 4,
+      "name": "Sully",
+      "breed": "Terrier/ Pit Mix",
+      "locationId": 2,
+      "treatment": "Lots of petting",
+      "customerId": 5
+    },
+    {
+      "id": 6,
+      "name": "Gracie",
+      "breed": "Another Doodle",
+      "locationId": 1,
+      "treatment": "Fatty lipoma removed",
+      "customerId": 5
+    },
+    {
+      "id": 7,
+      "name": "Shitty",
+      "breed": "Kitty",
+      "locationId": 2,
+      "treatment": "Behavioral treatment to be less shitty ",
+      "customerId": 5
+    }
+]
+
+def get_all_animals():
+    return ANIMALS
