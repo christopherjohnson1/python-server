f002ec4c01331e85a30af6bcc93c14a01cdf8596
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-07 16:58:02 -0400

    post works for all endpoints

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,9 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
-from locations import get_all_locations, get_single_location
-from employees import get_all_employees, get_single_employee
-from customers import get_all_customers, get_single_customer
+from animals import get_all_animals, get_single_animal, create_animal
+from customers import get_all_customers, get_single_customer, create_customer
+from employees import get_all_employees, get_single_employee, create_employee
+from locations import get_all_locations, get_single_location, create_location
 
 # Here's a class. It inherits from another class.
 class HandleRequests(BaseHTTPRequestHandler):
@@ -75,13 +76,35 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_resource = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_resource = create_animal(post_body)
+        
+        if resource == "customers":
+            new_resource = create_customer(new_resource)
+        
+        if resource == "employees":
+            new_resource = create_employee(post_body)
+        
+        if resource == "locations":
+            new_resource = create_location(post_body)
+        
+        self.wfile.write(f"{new_resource}".encode())
 
 
     # Here's a method on the class that overrides the parent's method.
@@ -96,3 +119,6 @@ def main():
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
+
+if __name__ == "__main__":
+    main()
