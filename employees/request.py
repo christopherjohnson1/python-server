16c853e935a48bbbe7d7d528dbedceec7c9b202b
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-07 14:22:00 -0400

    wip

---
+++ employees/request.py
@@ -0,0 +1,25 @@
+EMPLOYEES = [
+    {
+      "name": "Kurt Russel",
+      "locationId": 3,
+      "animalId": 3,
+      "id": 6
+    },
+    {
+      "name": "Chris Johnson",
+      "locationId": 3,
+      "animalId": 3,
+      "id": 7
+    }
+]
+
+def get_all_employees():
+    return EMPLOYEES
+
+def get_single_employee(id):
+    requested_employee = None
+    for employee in EMPLOYEES:
+        if employee{"id"} == id:
+            requested_employee = employee
+
+    return requested_employee
