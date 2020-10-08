606dc3fdf9916ec16c1d279a18dd922faaceff53
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-08 17:45:27 -0500

    update customers works

--- customers/request.py
+++ customers/request.py
@@ -2,30 +2,44 @@ CUSTOMERS = [
     {
       "id": 1,
       "name": "Hannah Hall",
-      "address": "7002 Chestnut Ct."
+      "address": "7002 Chestnut Ct.",
+      "status": "Loyal Customer"
     },
     {
       "id": 2,
       "name": "Mike Cool",
-      "address": "4931 Stormy Dr."
+      "address": "4931 Stormy Dr.",
+      "status": "Loyal Customer"
     },
     {
       "id": 3,
       "name": "Samantha Blackwell",
-      "address": "2945 River Rd."
+      "address": "2945 River Rd.",
+      "status": "Loyal Customer"
     },
     {
       "id": 4,
       "name": "David McDavidson",
-      "address": "731 Main St."
+      "address": "731 Main St.",
+      "status": "Loyal Customer"
     },
     {
       "email": "christopherjohnson1@gmail.com",
       "password": "me",
       "name": "Chris Johnson",
-      "id": 5
+      "id": 5,
+      "status": "Loyal Customer"
     }
 ]
+
+def update_customer(id, new_customer):
+    # Iterate the CUSTOMERS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the customer. Update the value.
+            CUSTOMERS[index] = new_customer
+            break
 
 def delete_customer(id):
     # Initial -1 value for customer index, in case one isn't found
