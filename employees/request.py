a150f848e8a85d2595b686412c5e8bb141e4d69c
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-08 17:14:13 -0500

    employees delete works

--- employees/request.py
+++ employees/request.py
@@ -13,6 +13,21 @@ EMPLOYEES = [
     }
 ]
 
+def delete_employee(id):
+    # Initial -1 value for employee index, in case one isn't found
+    employee_index = -1
+
+    # Iterate the EMPLOYEES list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            # Found the employee. Store the current index.
+            employee_index = index
+
+    # If the employee was found, use pop(int) to remove it from list
+    if employee_index >= 0:
+        EMPLOYEES.pop(employee_index)
+
 def create_employee(employee):
     # Get the id value of the last animal in the list
     max_id = EMPLOYEES[-1]["id"]
--- request_handler.py
+++ request_handler.py
@@ -2,7 +2,7 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from locations import get_all_locations, get_single_location, create_location, delete_location
 
 # Here's a class. It inherits from another class.
@@ -120,6 +120,10 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Delete a single customer from the list
         if resource == "customers":
             delete_customer(id)
+        
+        # Delete a single employee from the list
+        if resource == "employees":
+            delete_employee(id)
         
         # Delete a single location from the list
         if resource == "locations":
