03fd6f75f4250bdebe0c42a5d41a58388df555d4
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-09 12:14:36 -0400

    classes added

---
+++ models/employee.py
@@ -0,0 +1,8 @@
+from os import name
+
+
+class Employee():
+    def __init__(self, name, location_id, animal_id):
+        self.name = name
+        self.location_id = location_id
+        self.animal_id = animal_id
